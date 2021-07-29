99414ffe330ad7766f709d7615348e2ee90cf69e
Curtis1509 <contact.curtisjones@gmail.com>
2021-07-29 13:21:30 +1000

    First commit

---
+++ src/comp3230/javateam/fireolympics/FireOlympics.java
@@ -0,0 +1,23 @@
+package comp3230.javateam.fireolympics;
+
+public class FireOlympics implements Runnable{
+
+    boolean running;
+    private Thread thread;
+
+    public static void main(String [] args){
+        new FireOlympics();
+    }
+
+    public FireOlympics(){
+        running = true;
+        thread = new Thread(this, "Display");
+        thread.start();
+    }
+    public void run(){
+        while (running){
+
+        }
+    }
+
+}
