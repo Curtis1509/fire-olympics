1a093b4fcaf215c4d7af5973577d7f4a1498f945
Andyggg2 <andyggg2@outlook.com>
2021-08-25 12:58:11 +1000

    Fixed broken texture loading

--- app/src/main/java/fire/olympics/graphics/Texture.java
+++ app/src/main/java/fire/olympics/graphics/Texture.java
@@ -13,30 +13,15 @@ import org.lwjgl.system.MemoryUtil;
 public class Texture {
     int id;
 
-    @SuppressWarnings("StatementWithEmptyBody")
     public Texture(Path path) {
-        ByteBuffer imb;
-
-        try {
-            SeekableByteChannel fc = Files.newByteChannel(path);
-            imb = MemoryUtil.memAlloc((int)fc.size() + 1);
-            while(fc.read(imb) != -1) ;
-        } catch(Exception e) {
-            id = 0;
-            System.out.printf("Texture: %s%n", e);
-            return;
-        }
-
-        imb.flip();
-
         IntBuffer w = MemoryUtil.memAllocInt(1);
         IntBuffer h = MemoryUtil.memAllocInt(1);
         IntBuffer comp = MemoryUtil.memAllocInt(1);
 
-        STBImage.stbi_info_from_memory(imb, w, h, comp);
-        ByteBuffer im = STBImage.stbi_load_from_memory(imb, w, h, comp, 0);
+        STBImage.stbi_info(path.toAbsolutePath().toString(), w, h, comp);
+        ByteBuffer imageData = STBImage.stbi_load(path.toAbsolutePath().toString(), w, h, comp, STBImage.STBI_rgb);
 
-        if(im == null) {
+        if(imageData == null) {
             id = 0;
             System.out.printf("Texture: %s%n", STBImage.stbi_failure_reason());
             return;
@@ -44,10 +29,10 @@ public class Texture {
 
         id = glGenTextures();
         bind();
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w.get(0), h.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, im);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w.get(0), h.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
         glGenerateMipmap(GL_TEXTURE_2D);
         unbind();
-        STBImage.stbi_image_free(im);
+        STBImage.stbi_image_free(imageData);
     }
 
     public boolean imageLoaded() {
