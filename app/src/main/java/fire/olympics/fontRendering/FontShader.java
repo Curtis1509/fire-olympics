eeefca7a038e607c4a11a08cb9c372c24530dee4
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2021-09-01 17:01:48 +1000

    Converted tabs to spaces

--- app/src/main/java/fire/olympics/fontRendering/FontShader.java
+++ app/src/main/java/fire/olympics/fontRendering/FontShader.java
@@ -5,34 +5,34 @@ import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 public class FontShader extends ShaderProgram{
-	
-	private int location_colour;
-	private int location_translation;
-	
-	public FontShader() {
-		super(App.resource("shaders", "shader_for_text.vert").toString(),
-				App.resource("shaders", "shader_for_text.frag").toString());
-	}
+    
+    private int location_colour;
+    private int location_translation;
+    
+    public FontShader() {
+        super(App.resource("shaders", "shader_for_text.vert").toString(),
+                App.resource("shaders", "shader_for_text.frag").toString());
+    }
 
-	@Override
-	protected void getAllUniformLocations() {
-		location_colour = super.getUniformLocation("colour");
-		location_translation = super.getUniformLocation("translation");
-	}
+    @Override
+    protected void getAllUniformLocations() {
+        location_colour = super.getUniformLocation("colour");
+        location_translation = super.getUniformLocation("translation");
+    }
 
-	@Override
-	protected void bindAttributes() {
-		super.bindAttribute(0, "position");
-		super.bindAttribute(1, "textureCoords");
-	}
-	
-	public void loadColour(Vector3f colour){
-		super.loadVector(location_colour, colour);
-	}
-	
-	public void loadTranslation(Vector2f translation){
-		super.load2DVector(location_translation, translation);
-	}
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+        super.bindAttribute(1, "textureCoords");
+    }
+    
+    public void loadColour(Vector3f colour){
+        super.loadVector(location_colour, colour);
+    }
+    
+    public void loadTranslation(Vector2f translation){
+        super.load2DVector(location_translation, translation);
+    }
 
 
 }
--- app/src/main/java/fire/olympics/fontRendering/TextMaster.java
+++ app/src/main/java/fire/olympics/fontRendering/TextMaster.java
@@ -16,91 +16,91 @@ import org.lwjgl.opengl.*;
 
 public class TextMaster {
 
-	private static final List<Integer> vbos = new ArrayList<>();
-	private static final Map<FontType, List<GUIText>> texts = new HashMap<>();
-	private static FontRenderer renderer;
-	private static final List<Integer> textures = new ArrayList<>();
-	
-	public static void init(){
-		renderer = new FontRenderer();
-	}
+    private static final List<Integer> vbos = new ArrayList<>();
+    private static final Map<FontType, List<GUIText>> texts = new HashMap<>();
+    private static FontRenderer renderer;
+    private static final List<Integer> textures = new ArrayList<>();
+    
+    public static void init(){
+        renderer = new FontRenderer();
+    }
 
-	public static int loadToVAO(float[] positions, float[] textureCoords) {
-		int vaoID = createVAO();
-		storeDataInAttributeList(0, 2, positions);
-		storeDataInAttributeList(1, 2, textureCoords);
-		unbindVAO();
-		return vaoID;
-	}
-	private static void unbindVAO() {
-		GL30.glBindVertexArray(0);
-	}
+    public static int loadToVAO(float[] positions, float[] textureCoords) {
+        int vaoID = createVAO();
+        storeDataInAttributeList(0, 2, positions);
+        storeDataInAttributeList(1, 2, textureCoords);
+        unbindVAO();
+        return vaoID;
+    }
+    private static void unbindVAO() {
+        GL30.glBindVertexArray(0);
+    }
 
-	private static void storeDataInAttributeList(int attributeNumber, int coordinateSize, float[] data) {
-		int vboID = GL15.glGenBuffers();
-		vbos.add(vboID);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
-		FloatBuffer buffer = storeDataInFloatBuffer(data);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
-		GL20.glVertexAttribPointer(attributeNumber, coordinateSize, GL11.GL_FLOAT, false, 0, 0);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-	}
+    private static void storeDataInAttributeList(int attributeNumber, int coordinateSize, float[] data) {
+        int vboID = GL15.glGenBuffers();
+        vbos.add(vboID);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+        FloatBuffer buffer = storeDataInFloatBuffer(data);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(attributeNumber, coordinateSize, GL11.GL_FLOAT, false, 0, 0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+    }
 
-	private static FloatBuffer storeDataInFloatBuffer(float[] data) {
-		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
-		buffer.put(data);
-		buffer.flip();
-		return buffer;
-	}
+    private static FloatBuffer storeDataInFloatBuffer(float[] data) {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+        return buffer;
+    }
 
-	private static int createVAO() {
-		int vaoID = GL30.glGenVertexArrays();
-		GL30.glBindVertexArray(vaoID);
-		return vaoID;
-	}
-	
-	public static void render(){
-		renderer.render(texts);
-	}
+    private static int createVAO() {
+        int vaoID = GL30.glGenVertexArrays();
+        GL30.glBindVertexArray(vaoID);
+        return vaoID;
+    }
+    
+    public static void render(){
+        renderer.render(texts);
+    }
 
-	public static Texture texture;
+    public static Texture texture;
 
-	public static int loadTexture(String fileName) throws IOException {
-		texture = Texture.loadPNGTexture(fileName);
-		try {
-			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
-			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
-					GL11.GL_LINEAR_MIPMAP_LINEAR);
-			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0f);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("Tried to load texture " + fileName + ".png , didn't work");
-			System.exit(-1);
-		}
-		textures.add(texture.getId());
-		System.out.println(texture.getId());
-		return texture.getId();
-	}
+    public static int loadTexture(String fileName) throws IOException {
+        texture = Texture.loadPNGTexture(fileName);
+        try {
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
+                    GL11.GL_LINEAR_MIPMAP_LINEAR);
+            GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0f);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Tried to load texture " + fileName + ".png , didn't work");
+            System.exit(-1);
+        }
+        textures.add(texture.getId());
+        System.out.println(texture.getId());
+        return texture.getId();
+    }
 
-	public static void loadText(GUIText text){
-		FontType font = text.getFont();
-		TextMeshData data = font.loadText(text);
-		int vao = loadToVAO(data.getVertexPositions(), data.getTextureCoords());
-		text.setMeshInfo(vao, data.getVertexCount());
-		List<GUIText> textBatch = texts.computeIfAbsent(font, k -> new ArrayList<>());
-		textBatch.add(text);
-	}
-	
-	public static void removeText(GUIText text){
-		List<GUIText> textBatch = texts.get(text.getFont());
-		textBatch.remove(text);
-		if(textBatch.isEmpty()){
-			texts.remove(texts.get(text.getFont()));
-		}
-	}
-	
-	public static void cleanUp(){
-		renderer.cleanUp();
-	}
+    public static void loadText(GUIText text){
+        FontType font = text.getFont();
+        TextMeshData data = font.loadText(text);
+        int vao = loadToVAO(data.getVertexPositions(), data.getTextureCoords());
+        text.setMeshInfo(vao, data.getVertexCount());
+        List<GUIText> textBatch = texts.computeIfAbsent(font, k -> new ArrayList<>());
+        textBatch.add(text);
+    }
+    
+    public static void removeText(GUIText text){
+        List<GUIText> textBatch = texts.get(text.getFont());
+        textBatch.remove(text);
+        if(textBatch.isEmpty()){
+            texts.remove(texts.get(text.getFont()));
+        }
+    }
+    
+    public static void cleanUp(){
+        renderer.cleanUp();
+    }
 
 }
