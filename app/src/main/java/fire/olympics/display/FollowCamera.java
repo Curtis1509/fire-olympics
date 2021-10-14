da07504a33cb7850f0adb4006d1281892bd103b6
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2021-10-15 10:53:51 +1100

    Fix bugs in FollowCamera calculations

--- app/src/main/java/fire/olympics/display/FollowCamera.java
+++ app/src/main/java/fire/olympics/display/FollowCamera.java
@@ -3,19 +3,12 @@ package fire.olympics.display;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_D;
 
-import org.joml.Vector3f;
-
 public class FollowCamera extends Camera {
     private final Window window;
     private final float distanceFromTarget = 15;
 
     public GameItemGroup target;
-
-    private Vector3f position = new Vector3f(0, 0, 0);
-    private Vector3f angle = new Vector3f(0, 0, 0);
-
-    private float pitch = 0;
-    private float yaw = 0;
+    public float arrowSpeed = 25.0f;
 
     private float angleAboveArrow = 0;
     private float angleAroundArrow = 0;
@@ -32,44 +25,42 @@ public class FollowCamera extends Camera {
     /**
      * Allows camera to move in relation to arrow
      */
-    public void moveCamera() {
-        this.pitch = target.getRotation().x + angleAboveArrow;
-        this.yaw = (180 - target.getRotation().y) + angleAroundArrow;
-        angle.set(pitch, yaw, 0);
+    private void moveCamera() {
+        System.out.printf("%.3f, %.3f%n", angleAboveArrow, angleAroundArrow);
+        float pitch = target.getRotation().x + angleAboveArrow;
+        float yaw = (180 - target.getRotation().y) + angleAroundArrow;
 
         // Calculates the position the camera needs to be in to look at the object Using
         // the objects rotation and position The position is calculated as an offset of
         // the objects position tasking into account the x and y rotations
-        float horizontalDistance = calculateHorizontalDistance();
-        float verticalDistance = calculateVerticalDistance();
+        float horizontalDistance = (float) (distanceFromTarget * Math.cos(Math.toRadians(pitch)));
+        float verticalDistance = (float) (distanceFromTarget * Math.sin(Math.toRadians(pitch)));
         float theta = target.getRotation().y() + angleAroundArrow;
         float offsetX = (float) (horizontalDistance * Math.sin(Math.toRadians(theta)));
         float offsetZ = (float) (horizontalDistance * Math.cos(Math.toRadians(theta)));
         position.x = target.getPosition().x - offsetX;
         position.z = target.getPosition().z - offsetZ;
         position.y = target.getPosition().y + verticalDistance;
-        super.position.set(position);
-        super.rotation.set(angle);
+        rotation.set(-pitch, -yaw, 0);
     }
 
-
-    @Override
-    public void update(double timeDelta) {
-        processKeyBindings(timeDelta);
-        // double arrowSpeed = 25;
-
-        // Move player
-        // float dx = (float) ((arrowSpeed * timeDelta) * Math.sin(Math.toRadians(target.getRotation().x)));
-        // float dy = (float) ((arrowSpeed * timeDelta) * Math.sin(Math.toRadians(target.getRotation().y)));
-        // float dz = (float) ((arrowSpeed * timeDelta) * Math.cos(Math.toRadians(target.getRotation().z)));
-        // target.movePosition(dx, dy, dz);
-        moveCamera();
-    }
 
     /**
      * Allows follow camera control
      * @param timeDelta normalised frame time difference
      */
+    @Override
+    public void update(double timeDelta) {
+        processKeyBindings(timeDelta);
+        // Move player
+        float dx = (float) ((arrowSpeed * timeDelta) * Math.sin(Math.toRadians(target.getRotation().y)));
+        float dy = (float) ((arrowSpeed * timeDelta) * Math.sin(Math.toRadians(target.getRotation().x)));
+        float dz = (float) ((arrowSpeed * timeDelta) * Math.cos(Math.toRadians(target.getRotation().y)));
+        target.movePosition(dx, -dy, dz);
+        moveCamera();
+    }
+
+
     private void processKeyBindings(double timeDelta) {
         // Up and Down (Pitch) control
         if (window.isKeyDown(GLFW_KEY_W)) {
@@ -134,23 +125,5 @@ public class FollowCamera extends Camera {
                 angleAroundArrow += (float) (timeDelta * 10f);
             }
         }
-    }
-
-    /**
-     * Calculates the cameras horizontal distance from the arrow
-     * Using the camera pitch to match the rotation of the arrow
-     * @return Horizontal Distance to arrow
-     */
-    private float calculateHorizontalDistance() {
-        return (float) (distanceFromTarget * Math.cos(Math.toRadians(pitch)));
-    }
-
-    /**
-     * Calculates the cameras vertical distance from the arrow
-     * Using the camera pitch to match the rotation of the arrow
-     * @return Vertical Distance to arrow
-     */
-    private float calculateVerticalDistance() {
-        return (float) (distanceFromTarget * Math.sin(Math.toRadians(pitch)));
     }
 }
